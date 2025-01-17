37a9eeb2f30b22c06fb512613af61e984596ee01
manu2345-Cpu <manu111dwivedi@gmail.com>
2025-01-18 00:22:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ from subprocess import getstatusoutput
 import logging
 import os
 import re
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Set to an open port
 
 import requests
 bot = Client(
