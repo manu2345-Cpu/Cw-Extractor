5f3cb720d832096fb8c49fa1ad21f3d4b7d4fc26
manu2345-Cpu <manu111dwivedi@gmail.com>
2025-01-17 23:34:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ import requests
 bot = Client(
     "CW",
     bot_token=os.environ.get("BOT_TOKEN"),
-    api_id_=os.environ.get("API_ID"),
+    api_id=os.environ.get("API_ID"),
     api_hash=os.environ.get("API_HASH")
 )
 
