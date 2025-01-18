5a44890f995bb57279825ad7b4ea9c6f28dae804
manu2345-Cpu <manu111dwivedi@gmail.com>
2025-01-18 15:07:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ bot = Client(
     bot_token=os.environ.get("BOT_TOKEN"),
     api_id=os.environ.get("API_ID"),
     api_hash=os.environ.get("API_HASH")
+    port=os.environment.get("PORT")
 )
 
 logger = logging.getLogger()
